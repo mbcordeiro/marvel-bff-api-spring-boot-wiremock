15170d8eb089e1628bcd164675b9ea3fe70bf9bf
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2022-03-06 19:21:29 -0300

    First commit

---
+++ src/main/java/com/matheuscordeiro/bffmarvelapi/BffMarvelApiApplication.java
@@ -0,0 +1,13 @@
+package com.matheuscordeiro.bffmarvelapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffMarvelApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffMarvelApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheuscordeiro/bffmarvelapi/BffMarvelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheuscordeiro.bffmarvelapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffMarvelApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
